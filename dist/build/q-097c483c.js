c1583a81ee5b2e31d4e21a452465cb33b96949d5
Leo Tiofan Justicia <general@leotj.com>
2022-11-16 19:13:14 +0700

    chore: add dist directory

---
+++ dist/build/q-097c483c.js
@@ -0,0 +1,1 @@
+import{U as r,H as n,B as t}from"./q-f0e5f2d3.js";const e=()=>r(t,{children:r("main",{children:r("section",{children:r(n,{})})})});export{e as s_Xz5zrmRDQ2c};
---
+++ dist/build/q-50227c5f.js
@@ -0,0 +1,1 @@
+import{r as e}from"./q-f0e5f2d3.js";export{e as default};
